a54e279ca6252ea1d195dc060280e54ffce00d06
Vladimir <black-rage@mail.ru>
2021-05-23 18:32:00 +0300

    changed orders

---
+++ src/main/java/bot/botApi/handlers/CheckoutHandler.java
@@ -0,0 +1,39 @@
+package bot.botApi.Checkout;
+
+import bot.entities.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.send.SendInvoice;
+import org.telegram.telegrambots.meta.api.objects.payments.LabeledPrice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bot.service.ProductService;
+
+@Slf4j
+public class CheckoutHandler {
+    private SendInvoice sendInvoice;
+//    private ProductService productService = new ProductService();
+
+    public CheckoutHandler(){}
+
+    public SendInvoice preCheckout(long userId){
+        log.info("checkout started");
+        List<LabeledPrice> labeledPrice = new ArrayList<>();
+        List<Product> listOfProducts = new ArrayList<>(ProductService.getCard().get(userId));
+        listOfProducts.forEach(product -> {
+            //цена должна быть не менее 75 рублей
+            //АККУРАТНО
+            //здесь цена в копейках, поэтому домножается на сто
+            //если инвойс не присылается, и никаких ошибок - цена либо слишком большая, либо слишком маленькая
+            LabeledPrice l = new LabeledPrice(product.getName(), (int)product.getPrice() * 100);
+            labeledPrice.add(l);
+        });
+
+        sendInvoice = new SendInvoice(String.valueOf((int)userId), "2", "3", "4", "284685063:TEST:ZTEyMWRiZmU2YzQ0","true" , "RUB" , labeledPrice,
+                null,null, null, null, true, true, true, true, null, null, null,
+                null, null, null, null, null, null, null);
+        log.info("Invoice {}", sendInvoice);
+        return sendInvoice;
+    }
+}
